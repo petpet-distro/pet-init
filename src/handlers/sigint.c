b8f7463e34e03611139bbd913f5ee038dc08a2d9
Eqilia Luminé <eqilia@national.shitposting.agency>
2024-08-29 14:05:11 +0200

    I understand my enthusiasm, but this shouldn't be in PID 1...

--- src/handlers/sigint.c
+++ src/handlers/sigint.c
@@ -3,15 +3,9 @@
 #include <sys/reboot.h>
 #include <linux/reboot.h>
 
-#include "../../headers/src/helpers/killingSpree.h"
-
 void
 sigint(int signo)
 {
-	killingSpree();
-
-	sync();
-
 	reboot(LINUX_REBOOT_CMD_RESTART);
 	
 	return;
